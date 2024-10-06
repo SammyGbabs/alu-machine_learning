a475d36901e7cac4ceea62155020c995d7fccb76
SammyGbabs <s.babalola@alustudent.com>
2024-10-06 09:57:54 -0700

    1-sensitivity.py

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -6,4 +6,4 @@ import numpy as np
 
 def sensitivity(confusion):
     """Calculating the sensitivity for each class in a confusion matrix"""
-    return np.diag(confusion) / np.sum(confusion, axis=1
+    return np.diag(confusion) / np.sum(confusion, axis=1)
