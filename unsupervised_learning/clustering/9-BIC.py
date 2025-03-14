3263692482892ff7a4a461badaeb55f8b4d31257
SammyGbabs <s.babalola@alustudent.com>
2025-03-14 04:45:01 -0700

    9-BIC.py

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-"""This module contains a function that perfoms
-finds the best number of clusters for a GMM using the
-Bayesian Information Criterion"""
+"""This module contains a function that finds the best number of clusters
+for a GMM using the Bayesian Information Criterion"""
 import numpy as np
 expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     """
-    finds the best number of clusters for a GMM using the
+    Finds the best number of clusters for a GMM using the
     Bayesian Information Criterion
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
@@ -17,10 +16,13 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     if not isinstance(kmin, int) or kmin < 1:
         return None, None, None, None
 
-    if not isinstance(kmax, int) or kmax < kmin:
+    if kmax is None:
+        kmax = X.shape[0]
+
+    if not isinstance(kmax, int) or kmax <= kmin:
         return None, None, None, None
 
-    if not isinstance(iterations, int):
+    if not isinstance(iterations, int) or iterations <= 0:
         return None, None, None, None
 
     if not isinstance(tol, float) or tol < 0:
@@ -29,24 +31,30 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     if not isinstance(verbose, bool):
         return None, None, None, None
 
-    if kmax is None:
-        kmax = iterations
+    n, d = X.shape
+    likelihoods = []
+    bics = []
+    best_k = None
+    best_result = None
+    best_bic = float('inf')
 
-    n = X.shape[0]
-    prior_bic = 0
-    likelyhoods = bics = []
-    best_k = kmax
-    pi_prev = m_prev = S_prev = best_res = None
     for k in range(kmin, kmax + 1):
         pi, m, S, g, ll = expectation_maximization(X, k, iterations, tol,
-                                                   verbose)
-        bic = k * np.log(n) - 2 * ll
-        if np.isclose(bic, prior_bic) and best_k >= k:
-            best_k = k - 1
-            best_res = pi_prev, m_prev, S_prev
-        pi_prev, m_prev, S_prev = pi, m, S
-        likelyhoods.append(ll)
-        bics.append(bic)
-        prior_bic = bic
+                                                 verbose)
+        if pi is None:
+            return None, None, None, None
 
-    return best_k, best_res, np.asarray(likelyhoods), np.asarray(bics)
+        p = (k - 1) + (k * d) + (k * d * (d + 1) // 2)
+        bic = p * np.log(n) - 2 * ll
+        likelihoods.append(ll)
+        bics.append(bic)
+
+        if bic < best_bic:
+            best_k = k
+            best_result = (pi, m, S)
+            best_bic = bic
+
+    if best_k is None:
+        return None, None, None, None
+
+    return best_k, best_result, np.array(likelihoods), np.array(bics)
